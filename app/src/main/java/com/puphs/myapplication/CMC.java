dedc0ad195d270eb8bdb778418b4b83a7290a88d
yuzuReason <roy59136842@gmail.com>
2018-04-14 15:54:54 +0800

    test

--- app/src/main/java/com/puphs/myapplication/CMC.java
+++ app/src/main/java/com/puphs/myapplication/CMC.java
@@ -8,6 +8,6 @@ public class CMC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cmc);
+//        setContentView(R.layout.activity_cmc);
     }
 }
